7cca7345b6048c70ecd7cddf69c30aeb0b82dc92
dragonpilot <dgnpilot@gmail.com>
2020-02-06 21:50:56 +1000

    rename en to enUS

--- offroad/js/utils/I18n.js
+++ offroad/js/utils/I18n.js
@@ -1,12 +1,12 @@
 import { NativeModules } from 'react-native'
 import { setupI18n } from "@lingui/core"
-import en from '../locales/en/messages.js';
+import enUS from '../locales/en_US/messages.js';
 import zhTW from '../locales/zh_TW/messages.js';
 import zhCN from '../locales/zh_CN/messages.js';
 
 // define which locale use which translate file
 let supportedLanguage = {
-    'en': en,
+    'en_US': enUS,
     'zh_TW': zhTW,
     'zh_CN': zhCN,
 };
@@ -19,7 +19,7 @@ i18n.load(supportedLanguage);
 if (supportedLanguage.hasOwnProperty(locale)) {
     i18n.activate(locale);
 } else {
-    i18n.activate('en');
+    i18n.activate('en_US');
 }
 
 export default i18n
