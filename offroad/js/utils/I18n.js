47a7e1f4a69b63eb0b1130482d2aeb1ded30d840
dragonpilot <dgnpilot@gmail.com>
2020-02-18 12:39:07 +1000

    Use moments for date time

--- offroad/js/utils/I18n.js
+++ offroad/js/utils/I18n.js
@@ -3,7 +3,9 @@ import { setupI18n } from "@lingui/core"
 import enUS from '../locales/en_US/messages.js';
 import zhTW from '../locales/zh_TW/messages.js';
 import zhCN from '../locales/zh_CN/messages.js';
-
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'moment/locale/zh-tw';
 // define which locale use which translate file
 let supportedLanguage = {
     'en_US': enUS,
@@ -18,8 +20,10 @@ i18n.load(supportedLanguage);
 // if the locale is defined, activate it
 if (supportedLanguage.hasOwnProperty(locale)) {
     i18n.activate(locale);
+    moment.locale(locale);
 } else {
     i18n.activate('en_US');
+    moment.locale('en_US')
 }
 
 export default i18n
